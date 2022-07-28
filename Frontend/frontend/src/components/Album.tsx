0ae73c3d70cef9e8fac3105d3ee1085635c61ac1
Patrick Buck <pbuck@wayfair.com>
2022-07-28 11:25:51 -0400

    Solved communication issue, js runs from the browser and not from the server. You cant reference the backend dns from the browser.

--- Frontend/frontend/src/components/Album.tsx
+++ Frontend/frontend/src/components/Album.tsx
@@ -1,5 +1,4 @@
-import React from "react";
-import axios from 'axios'; 
+import React, { useEffect } from "react";
 interface inputAlbum {
     id : number,
     title : string,
@@ -13,35 +12,23 @@ type pathProp = {
 
 export default function Album(prop: pathProp) {
     const [albums, setAlbums] = React.useState<inputAlbum[]>([]);
-    // fetch(prop.path).then(res => res.json()).then(data => {
-    //     console.log(data);
-    //     setAlbums(data);
-    // });
-    // fetch('http://backend:8081').then( res => {
-    //     console.log(res.json());
-    // });
-    axios.get(prop.path).then(
-        res => {
-            console.log(res.data())
-        }
-    )
+    useEffect(()=> {
+        fetch(prop.path).then(res => res.json()).then(data => {
+            setAlbums(data);
+        });
+    }, []);
     return (
-        // <div>
-        //     {albums.map(album => {
-        //         return (
-        //             <div>
-        //                 <h1>{album.title}</h1>
-        //                 <h2>{album.artist}</h2>
-        //                 <h3>{album.price}</h3>
-        //             </div>
-        //         );
-        //     }
-        //     )}
-        // </div>
         <div>
-            <div>
-                Test
-            </div>
+            {albums.map(album => {
+                return (
+                    <div>
+                        <h1>{album.title}</h1>
+                        <h2>{album.artist}</h2>
+                        <h3>{album.price}</h3>
+                    </div>
+                );
+            }
+            )}
         </div>
     );
 }
